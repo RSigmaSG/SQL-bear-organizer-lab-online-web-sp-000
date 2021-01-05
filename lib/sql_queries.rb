07dd5e46a70661b1bc9de9e55be5109e4d76126f
Sumer   Gaikwad <sg.comparch@gmail.com>
2021-01-05 03:53:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ end
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
-
+def selects_youngest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
